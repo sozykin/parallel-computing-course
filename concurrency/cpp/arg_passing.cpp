0a8326816b9e6c22989269743264aad7196049ad
Andrey Sozykin <sozykin@gmail.com>
2015-02-09 01:01:09 +0500

    Adding agrument passing example

---
+++ concurrency/cpp/arg_passing.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <thread>
+#include <string>
+using namespace std;
+
+// Thread function with 2 arguments
+void thread_func(string const &question, string &answer){
+    cout << "The question: " << question << endl;
+    answer = "42";
+}
+
+int main(){
+    string answer;
+    string question("Life, the Universe and Everything");
+    // Passing arguments to the thread function in the thread constructor
+    // question is copied
+    // answer is pased by reference
+    thread t(thread_func, question, ref(answer));
+    t.join();
+    cout << "The answer: " << answer << endl;
+}
