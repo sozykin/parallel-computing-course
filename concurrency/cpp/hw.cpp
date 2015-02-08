bd319286084b1968666dec38ae1ffd06a157ecdd
Andrey Sozykin <sozykin@gmail.com>
2015-02-08 23:28:34 +0500

    adding comments to hw.cpp

--- concurrency/cpp/hw.cpp
+++ concurrency/cpp/hw.cpp
@@ -3,11 +3,15 @@
 using namespace std;
 
 void thread_func(){
+    // Pronting Hello world and ID of the thread
     cout << "Hello world from thread " << this_thread::get_id() << "!" << endl;
 }
 
 int main() {
+    // Creating new thread which will run the thread_func function
     thread t(thread_func);
+    // Waiting for thread to finish
     t.join();
+    // Printing ID of main thread
     cout << "Main thread id is " << this_thread::get_id() << endl;
 }
