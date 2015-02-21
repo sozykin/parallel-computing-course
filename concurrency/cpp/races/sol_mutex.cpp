9e60a845cd6604901fffae74913be414946bc65e
Andrey Sozykin <sozykin@gmail.com>
2015-02-21 18:07:39 +0500

    Adding atomic soluation to race conditions example

--- concurrency/cpp/races/sol_mutex.cpp
+++ concurrency/cpp/races/sol_mutex.cpp
@@ -10,8 +10,8 @@ using namespace std;
 
 int const THREAD_NUM = 20;
 
-void compute(double const x, double &result, mutex &result_mutex){
-    double temp = (sin(x) + cos(x)) / exp (x / 2);
+void compute(int const x, int &result, mutex &result_mutex){
+    int temp = x * x;
     // Lock mutex
     result_mutex.lock();
     // Write to shared variable
@@ -22,14 +22,14 @@ void compute(double const x, double &result, mutex &result_mutex){
 int main(){
     /// 
     /// Shared variable sum
-    double sum = 0;
+    int sum = 0;
     ///
     /// Mutex for sum
     /// You have to lock mutex befor writing to sum
     mutex sum_mutex;
     vector<thread> threads;
     for (int i = 0; i <= THREAD_NUM; i++)
-        threads.push_back(thread(compute, i / 10, ref(sum), ref(sum_mutex)));
+        threads.push_back(thread(compute, i, ref(sum), ref(sum_mutex)));
     
     for (thread& t : threads)
         t.join();
