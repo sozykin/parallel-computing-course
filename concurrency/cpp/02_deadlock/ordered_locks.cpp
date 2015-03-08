b27d43fd8b4f41a3308f96fc26dba6aaaae4345f
Andrey Sozykin <sozykin@gmail.com>
2015-03-09 00:10:36 +0500

    Adding example of acuiring locks in one order

---
+++ concurrency/cpp/02_deadlock/ordered_locks.cpp
@@ -0,0 +1,49 @@
+///
+/// Example of deadlock
+/// Acquiring locks in one order 
+///
+#include <iostream>
+#include <thread>
+#include <string>
+#include <mutex>
+#include <chrono>
+using namespace std;
+
+void stew(mutex& burner, mutex& mixer){
+
+    cout << "[stew] Locking Mixer\n";
+    lock_guard<mutex> ml(mixer);
+    cout << "[stew] Mixer is locked\n";
+
+    this_thread::sleep_for(chrono::milliseconds(100));
+
+    cout << "[stew] Locking Burner\n";
+    lock_guard<mutex> bl(burner);
+    cout << "[stew] Burner is locked\n";
+
+    cout << "[stew] Preparing\n";
+}
+
+void pudding(mutex& burner, mutex& mixer){
+ 
+    cout << "[pudding] Locking Mixer\n";
+    lock_guard<mutex> ml(mixer);
+    cout << "[pudding] Mixer is locked\n";
+   
+    this_thread::sleep_for(chrono::milliseconds(100));
+
+    cout << "[pudding] Locking Burner\n";
+    lock_guard<mutex> bl(burner);
+    cout << "[pudding] Burner is locked\n";
+
+    cout << "[pudding] Preparing\n";
+}
+
+int main(){
+    mutex burner, mixer;
+    thread stew_thread(stew, ref(burner), ref(mixer));
+    thread pudding_thread(pudding, ref(burner), ref(mixer));
+    stew_thread.join();
+    pudding_thread.join();
+}
+
