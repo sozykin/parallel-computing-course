71aa75ddac7bb653c99e6b8dda1d5a391304cf1a
Andrey Sozykin <sozykin@gmail.com>
2015-03-08 23:24:20 +0500

    Add more verbose output to deadlock example

--- concurrency/cpp/02_deadlock/deadlock.cpp
+++ concurrency/cpp/02_deadlock/deadlock.cpp
@@ -14,11 +14,13 @@ const string PUDDING_DESC = "[pudding] ";
 void stew(mutex& burner, mutex& mixer){
     cout << "[stew] Locking Burner" << endl;
     lock_guard<mutex> bl(burner);
+    cout << "[stew] Burner is locked" << endl;
 
     this_thread::sleep_for(chrono::milliseconds(100));
 
     cout << "[stew] Locking Mixer" << endl;
     lock_guard<mutex> ml(mixer);
+    cout << "[stew] Mixer is locked" << endl;
 
     cout << STEW_DESC << "[stew] Preparing" << endl;
 }
@@ -27,11 +29,13 @@ void pudding(mutex& burner, mutex& mixer){
  
     cout << "[pudding] Locking Mixer" << endl;
     lock_guard<mutex> ml(mixer);
+    cout << "[pudding] Mixer is locked" << endl;
    
     this_thread::sleep_for(chrono::milliseconds(100));
 
     cout << "[pudding] Locking Burner" << endl;
     lock_guard<mutex> bl(burner);
+    cout << "[pudding] Burner is locked" << endl;
 
     cout << "[pudding] Preparing" << endl;
 }
