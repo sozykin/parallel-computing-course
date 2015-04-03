28d4bade676c9ffb3305fc279077fe0f44fe1f53
Andrey Sozykin <sozykin@gmail.com>
2015-04-03 13:56:03 +0500

    Add mpi hello world example

---
+++ mpi/hw.c
@@ -0,0 +1,16 @@
+///
+/// Hello world MPI example
+///
+//
+#include "mpi.h"
+#include <stdio.h>
+
+int main(int argc, char **argv){
+    int rank, size;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    printf("Hello, world! Process %d of %d\n", rank, size);
+
+    MPI_Finalize();
+}
